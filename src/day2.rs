a93dd0b829a6fa4c06d089058f89a450dce544f4
Ruben Gees <rubengees7@gmail.com>
2025-07-19 12:01:06 +0200

    Add day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 pub fn part1(input: &str) -> String {
     let sum: i32 = input
         .lines()
@@ -10,7 +12,30 @@ pub fn part1(input: &str) -> String {
             let min = numbers.iter().min().unwrap_or(&0);
             let max = numbers.iter().max().unwrap_or(&0);
 
-            return max - min;
+            max - min
+        })
+        .sum();
+
+    sum.to_string()
+}
+
+pub fn part2(input: &str) -> String {
+    let sum: i32 = input
+        .lines()
+        .map(|line| {
+            let numbers: Vec<i32> = line
+                .split_whitespace()
+                .map(|num| num.parse().expect("Not a number"))
+                .collect();
+
+            numbers
+                .iter()
+                .permutations(2)
+                .find_map(|pair| match pair.as_slice() {
+                    [a, b] if **b != 0 && *a % *b == 0 => Some(*a / *b),
+                    _ => None,
+                })
+                .unwrap_or(0)
         })
         .sum();
 
@@ -26,5 +51,12 @@ mod tests {
         let input = "5 1 9 5\n7 5 3\n2 4 6 8";
 
         assert_eq!(part1(input), "18");
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "5 9 2 8\n9 4 7 3\n3 8 6 5";
+
+        assert_eq!(part2(input), "9");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() {
 
     let [part1, part2] = match day {
         1 => [day1::part1(contents), day1::part2(contents)],
-        2 => [day2::part1(contents), "Not implemented".into()],
+        2 => [day2::part1(contents), day2::part2(contents)],
         _ => {
             eprintln!("Day {} is not implemented yet", day);
             process::exit(1);
